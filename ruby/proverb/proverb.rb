a9dd42454d2f6ec63bec7e2e9acd1b11c36adf1f
Adam89 <Adamm1@aston.ac.uk>
2014-01-27 20:55:48 -0700

    added a lot of stuff

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -6,8 +6,16 @@ class Proverb
   end
 
   def to_s
-    "For want of a #{@nouns.first} the #{@nouns.last} was lost.\n" +
-      "And all for the want of a #{@nouns.first}."
+    @nouns.map.with_index do |noun, index|
+      first_part_of_chorus(noun) + second_part_of_chorus(@nouns[index+1])
+    end.join + " And all for the want of a #{@nouns.first}."
   end
 
+  def first_part_of_chorus(word)
+    "For want of a #{word}"
+  end
+
+  def second_part_of_chorus(word)
+    " the #{word} was lost.\n"
+  end
 end
