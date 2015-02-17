f85859d2661ae911bed903b570c1fb938a088906
Adam89 <adam.dev89@gmail.com>
2015-02-17 17:56:32 +0000

    Create basic merge_sort cism

--- ruby/merge-sort/merge_sort.rb
+++ ruby/merge-sort/merge_sort.rb
@@ -18,17 +18,22 @@ class MergeSort
 
   def merge(left, right)
     result = []
-    i = 0
-    while right.length > i || left.length > i
-      if right[i] == nil
-        result = result + left[i..-1]
+    zero = 0
+
+    while right.length > zero || left.length > zero
+      if right[zero] == nil
+        result = result + left[zero..-1]
         break
-      elsif left[i] == nil
-        result = result + right[i..-1]
+      elsif left[zero] == nil
+        result = result + right[zero..-1]
         break
-      elsif right[i] < left[i]
+      elsif left[zero] == result[-1]
+        left.shift
+      elsif right[zero] == result[-1]
+        right.shift
+      elsif right[zero] <= left[zero]
         result << right.shift
-      elsif left[i] < right[i]
+      elsif left[zero] <= right[zero]
         result << left.shift
       end
     end
--- ruby/merge-sort/merge_sort_test.rb
+++ ruby/merge-sort/merge_sort_test.rb
@@ -3,45 +3,65 @@ require 'minitest/pride'
 require_relative 'merge_sort'
 
 class MergeSortTest < MiniTest::Test
-  def test_it_sorts_for_base_case
+  def test_it_sorts_for_empty_array
     merge = MergeSort.new([])
 
     assert_equal [], merge.merge_sort
   end
 
-  def test_it_sorts_for_second_base_case
+  def test_it_sorts_for_array_with_one_element
     merge = MergeSort.new([1])
 
     assert_equal [1], merge.merge_sort
   end
 
-  def test_it_sorts_for_small_even_length_unordered_list
+  def test_it_sorts_for_small_even_length_unordered_array
     merge = MergeSort.new([2, 1])
 
     assert_equal [1, 2], merge.merge_sort
   end
 
-  def test_it_sorts_for_bigger_even_length_unordered_list
+  def test_it_sorts_for_bigger_even_length_unordered_array
     merge = MergeSort.new([1, 2, 4, 3])
 
     assert_equal [1, 2, 3, 4], merge.merge_sort
   end
 
-  def test_it_sorts_for_small_odd_length_unordered_list
+  def test_it_sorts_for_small_odd_length_unordered_array
     merge = MergeSort.new([3, 2, 1, 5, 4])
 
     assert_equal [1, 2, 3, 4, 5], merge.merge_sort
   end
 
-  def test_it_sorts_for_large_odd_length_unordered_list
-    merge = MergeSort.new([3, 2, 1, 5, 4,6 , 8])
+  def test_it_sorts_for_large_odd_length_unordered_array
+    merge = MergeSort.new([3, 2, 1, 5, 4, 6, 8])
 
     assert_equal [1, 2, 3, 4, 5, 6, 8], merge.merge_sort
   end
 
-  def test_it_sorts_for_largest_odd_length_unordered_list
+  def test_it_sorts_for_larger_odd_length_unordered_array
     merge = MergeSort.new([3, 2, 1, 5, 4, 6, 8, 11, 9, 10, 100, 43, 50])
 
     assert_equal [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 43, 50, 100], merge.merge_sort
+  end
+
+  def test_it_sorts_for_small_odd_length_unordered_array_with_duplicates
+    merge = MergeSort.new([3, 2, 1, 1, 2])
+
+    assert_equal [1, 2, 3], merge.merge_sort
+  end
+
+  def test_it_sorts_for_large_odd_length_unordered_array_with_duplicates
+    merge = MergeSort.new([3, 2, 1, 1, 5, 3, 4, 6, 8, 11, 9, 10, 100, 43, 50])
+
+    assert_equal [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 43, 50, 100], merge.merge_sort
+  end
+
+  def test_it_sorts_for_very_large_array
+    large_unsorted_array = (1..1_000_000).to_a.shuffle!
+    merge = MergeSort.new(large_unsorted_array)
+
+    expected = (1..1_000_000).to_a
+    assert_equal expected, merge.merge_sort
   end
 end
